790e9c7041dd0da7a13fb3789b1ae7e6c5d886af
AdamChb <chabaadam75@gmail.com>
2025-04-23 18:57:02 +0200

    Test CORS

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 // api/index.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello depuis Express.js sur Vercel ! 🚀');
 });
+
+app.use(cors({
+  origin: 'https://flash-green.arcktis.fr',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 module.exports = app;
