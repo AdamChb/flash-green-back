15ed988f8687f942eb6dadcc39b99779b7d37414
AdamChb <chabaadam75@gmail.com>
2025-04-23 19:00:56 +0200

    Test protec serveur

--- api/index.js
+++ api/index.js
@@ -7,11 +7,22 @@ app.get('/', (req, res) => {
   res.send('Hello depuis Express.js sur Vercel ! 🚀');
 });
 
-app.use(cors({
-  origin: 'https://flash-green.arcktis.fr',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'https://flash-green.arcktis.fr',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['https://flash-green.arcktis.fr'];
+  const origin = req.get('Origin');
+
+  if (origin && !allowedOrigins.includes(origin)) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+
+  next();
+});
 
 module.exports = app;
